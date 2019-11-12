480aad7eba81e537f4f0fe98cff1048cb93c7f69
Rogerio Benevento Jr <rogerbenevento@gmail.com>
2019-11-12 14:13:45 -0300

    Atualizado projeto para ter a traducao em ptbr

---
+++ admin/language/ptbr/shipping/freteclick.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']    = 'Freteclick';
+
+// Text
+$_['text_shipping']    = 'Freteclick Frete';
+$_['text_success']     = 'Sucesso: Você modificou o Freteclick com sucesso!';
+$_['text_edit']        = 'Editar Freteclick';
+
+// Entry
+$_['entry_total']      = 'Total';
+$_['entry_geo_zone']   = 'Regiões';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Ordem';
+
+// Help
+$_['help_total']       = 'Sub-Total amount needed before the Freteclick shipping module becomes available.';
+
+// Error
+$_['error_permission'] = 'Ooops: Você não tem permissão para modificao Freteclick!';
